cbce7d83044a617f2a0e16fb6a339a27a893785f
patnamtanishka07 <patnam tanishka@gmail.com>
2026-08-07 12:25:07 +0530

    Connect frontend to deployed backend

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
     setAnswer("");
 
     try {
-      const response = await fetch("http://localhost:8000/ask", {
+      const response = await fetch("https://askmynotes-app.onrender.com", {
         method: "POST",
 
         headers: {
